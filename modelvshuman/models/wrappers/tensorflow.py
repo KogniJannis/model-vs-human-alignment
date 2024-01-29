8bd161506e62000bee2f7d3e62203c5fe1bce055
KogniJannis <104796901+KogniJannis@users.noreply.github.com>
2024-01-29 23:55:03 +0100

    Add preprocessing for harmonized models

--- modelvshuman/models/wrappers/tensorflow.py
+++ modelvshuman/models/wrappers/tensorflow.py
@@ -33,3 +33,48 @@ class TensorflowModel(AbstractModel):
         with device:
             predictions = self.model(images)
             return predictions.numpy()
+            
+
+"""
+Preprocessing for harmonized models (Fel et al., 2022)
+Adapted from: https://github.com/serre-lab/Harmonization/blob/main/harmonization/models/preprocess.py
+(preprocess_input was directly adapted with constants being inserted directly; commentary left as is)
+see https://serre-lab.github.io/Harmonization/training/ for use case
+"""
+class HarmonizedTensorflowModel(TensorflowModel):
+    def __init(self, model, model_name, *args):
+        super(HarmonizedTensorflowModel, self).__init__(model, model_name, *args)
+        
+    def preprocess_input(images): 
+        """
+        Preprocesses images for the harmonized models.
+        The images are expected to be in RGB format with values in the range [0, 255].
+    
+        Parameters
+        ----------
+        images
+            Tensor or numpy array to be preprocessed.
+            Expected shape (N, W, H, C).
+    
+        Returns
+        -------
+        preprocessed_images
+            Images preprocessed for the harmonized models.
+        """
+        images = images / 255.0
+    
+        images = images - np.array([0.485, 0.456, 0.406]) #IMAGENET_MEAN
+        images = images /  np.array([0.229, 0.224, 0.225]) #IMAGENET_STD 
+    
+        return images
+        
+    def softmax(self, logits):
+        assert type(logits) is np.ndarray
+        return tf.nn.softmax(logits).numpy()
+
+    def forward_batch(self, images):
+        device = get_device()
+        with device:
+            images = self.preprocess_input(images)
+            predictions = self.model(images)
+            return predictions.numpy()
