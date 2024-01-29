468be2a4bb7065426e92aa8235e4721683def452
KogniJannis <104796901+KogniJannis@users.noreply.github.com>
2024-01-29 18:29:56 +0100

    Create harmonized_model_url.py
    sources for weights of the harmonized models from Serre Lab

---
+++ modelvshuman/models/tensorflow/harmonized_model_url.py
@@ -0,0 +1,9 @@
+harmonized_urls = {
+    'convnext_harmonized':'https://storage.googleapis.com/serrelab/prj_harmonization/models/convnext_tiny_harmonized.h5',
+    'efficient_harmonized':'https://storage.googleapis.com/serrelab/prj_harmonization/models/efficientnetB0_harmonized.h5',
+    'levit_harmonized':'https://storage.googleapis.com/serrelab/prj_harmonization/models/levit_small_harmonized.h5',
+    'maxvit_harmonized':'https://storage.googleapis.com/serrelab/prj_harmonization/models/maxvit_tiny_harmonized.h5',
+    'resnet_harmonized':'https://storage.googleapis.com/serrelab/prj_harmonization/models/resnet50v2_harmonized.h5',
+    'vgg_harmonized':'https://storage.googleapis.com/serrelab/prj_harmonization/models/vgg16_harmonized.h5',
+    'vit_harmonized':'https://storage.googleapis.com/serrelab/prj_harmonization/models/vit-b16_harmonized.h5'
+}
