b28b5e085c05619251b65fc0efa95f4a821020ef
KogniJannis <104796901+KogniJannis@users.noreply.github.com>
2024-02-05 12:03:17 +0100

    wrapper for effnets with resizing

--- modelvshuman/models/wrappers/tensorflow.py
+++ modelvshuman/models/wrappers/tensorflow.py
@@ -33,14 +33,37 @@ class TensorflowModel(AbstractModel):
         with device:
             predictions = self.model(images)
             return predictions.numpy()
-            
 
-"""
+'''
+Wrapper for Efficientnets b1-b7 which need image resizing
+(I'm scared of putting resize in the standard wrapper; this should ensure max. backward compatibility/reproducibility
+'''
+
+class TensorflowEfficientnetModel(TensorflowModel):
+
+    def __init__(self, model, model_name, effnet_resolution, *args):
+        self.model = model
+        self.model_name = model_name
+        self.args = args
+        self.effnet_resolution = effnet_resolution
+
+    def softmax(self, logits):
+        assert type(logits) is np.ndarray
+        return tf.nn.softmax(logits).numpy()
+
+    def forward_batch(self, images):
+        device = get_device()
+        with device:
+            images = tf.image.resize(images, [self.effnet_resolution, self.effnet_resolution])
+            predictions = self.model(images)
+            return predictions.numpy()
+
+'''
 Preprocessing for harmonized models (Fel et al., 2022)
 Adapted from: https://github.com/serre-lab/Harmonization/blob/main/harmonization/models/preprocess.py
 (preprocess_input was directly adapted with constants being inserted directly; commentary left as is)
 see https://serre-lab.github.io/Harmonization/training/ for use case
-"""
+'''
 class HarmonizedTensorflowModel(TensorflowModel):
     def __init(self, model, model_name, *args):
         super(HarmonizedTensorflowModel, self).__init__(model, model_name, *args)
